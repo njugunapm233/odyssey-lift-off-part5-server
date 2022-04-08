4dbe4504687788f8334756fa4529ba1a9a1b5f30
Peter Njuguna <peter.njuguna@kirkonulkomaanapu.fi>
2022-04-08 16:37:18 +0300

    length field deprecated

--- src/resolvers.js
+++ src/resolvers.js
@@ -39,12 +39,20 @@ const resolvers = {
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
+
+      
     },
 
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
+
+     /* Track.author and Track.modules resolvers */
+     durationInSeconds: ({length}) => length,
   },
+  Module:{
+    durationInSeconds:({length})=> length,
+  }
 };
 
 module.exports = resolvers;
